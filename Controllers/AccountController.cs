ea177a0e0b36a0785d625103e2a1c43fd4c7ac97
aeisolution <info@aeisolution.it>
2017-06-28 09:16:44 +0200

    Variazione Layout viewStart

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -387,8 +387,8 @@ namespace wtime.Controllers
 
         //
         // POST: /Account/LogOff
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
